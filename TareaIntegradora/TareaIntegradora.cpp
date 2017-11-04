d8c7dc0dc468bf3ad1015942d60674b6a6e6048b
Luis Felipe Alvarez <luisfeal1998@hotmail.com>
2017-11-04 15:25:29 -0600

    fill and show board functions created

--- TareaIntegradora/TareaIntegradora.cpp
+++ TareaIntegradora/TareaIntegradora.cpp
@@ -1,11 +1,49 @@
-// TareaIntegradora.cpp : Defines the entry point for the console application.
-//
+/*
+	Tarea Integradora
+	11/4/2017
+	Por Luis Felipe Alvarez Sanchez A01194173
 
+*/
 #include "stdafx.h"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+using namespace std;
 
+char board[4][4];
 
+void fillBoard() {
+	int counter, i , j;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			board[i][j] = ' ';
+		}
+	}
+	srand((int)time(0));
+	for (counter = 1; counter <= 13;) {
+		i = rand() % 4;
+		j = rand() % 4;
+		if (board[i][j] == ' ') {
+			board[i][j] = '*';
+			counter++;
+		}
+	}
+
+}
+void showBoard() {
+	int counter = 1;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			cout << '[' <<board[i][j] << ']' <<  " ";
+			counter++;
+		}
+		cout << " "<<"\n";
+	}
+}
 int main()
 {
+	fillBoard();
+	showBoard();
     return 0;
 }
 
